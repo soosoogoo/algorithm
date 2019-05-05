d06e5856d69033a25bfe6f8671aa51ec10c9f585
soosoogoo <soosoogoo@gmail.com>
2019-05-05 11:54:36 +0800

    test

---
+++ src/main/java/com/wx/a/TestDemo.java
@@ -0,0 +1,17 @@
+package com.wx.a;
+
+import org.junit.Test;
+
+public class TestDemo {
+    @Test
+    public void testa(){
+        System.out.println("a");
+    }
+
+    @Test
+    public void testb(){
+        System.out.println("b");
+    }
+
+}
+
